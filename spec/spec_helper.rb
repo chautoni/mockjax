77770b3ffe2487da3e24ad93fa1b45733b19f006
Thach Chau <rog.kane@gmail.com>
2013-07-03 19:19:47 +0700

    Resolve some issues so Rspec test can be run with newest version of Rspec & Capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
 require 'bundler'
 Bundler.require :default, :test
 
+
+require 'capybara'
 require 'capybara/rspec'
+require 'capybara/dsl'
 require 'capybara-webkit'
+require 'mockjax'
 
 require File.expand_path('../app/app.rb', __FILE__)
 
@@ -11,3 +15,7 @@ Capybara.app = Rack::Builder.new {
   use Rack::Mockjax
   run Sinatra::Application
 }
+
+RSpec.configure do |config|
+  config.include Mockjax::Helpers
+end
