12eafccec78f9a59e91611459d986449c8d5f73d
Thach Chau <rog.kane@gmail.com>
2013-07-03 19:20:58 +0700

    Resolve Rack body parsing issue

--- lib/mockjax/rack.rb
+++ lib/mockjax/rack.rb
@@ -8,16 +8,15 @@ class Rack::Mockjax
 
   def call(env)
     @status, @headers, @body = @app.call(env)
-    @body = @body.to_a.join
     insert!
-    [@status, @headers, [@body]]
+    [@status, @headers, @body]
   end
 
   def insert!
     mocks = ''.tap do |m|
       Mockjax.mocks.each { |mock| m << "$.mockjax(#{mock.to_json});\n" }
     end
-    @body.gsub!(/(<\/head>)/, "<script src='#{Mockjax.path_to_js}' type='text/javascript'></script>\n<script>#{mocks}</script>\\1")
-    @headers['Content-Length'] = Rack::Utils.bytesize(@body).to_s
+    @body = [@body.join.gsub!(/(<\/head>)/, "<script src='#{Mockjax.path_to_js}' type='text/javascript'></script>\n<script>#{mocks}</script>\\1")]
+    @headers['Content-Length'] = Rack::Utils.bytesize(@body.join.to_s).to_s
   end
 end
